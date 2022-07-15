1ba0ce95b6f9ccd4ebc539d54fe511fbba9ccf40
KamilSkrzynski <kamilskrzynski95@gmail.com>
2022-07-15 18:07:06 +0200

    Initial commit

---
+++ SFSymbols/SFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsApp.swift
+//  SFSymbols
+//
+//  Created by Kamil Skrzyński on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
